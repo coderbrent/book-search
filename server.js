5457c082fdba43533a1e92aa97cb26b02abdb33b
Brent Abruzese <brent.abruzese@gmail.com>
2019-08-01 12:16:43 -0400

    removed cra boilerplate files and created component library for book search view

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3001;
 const app = express();
 
-app.get('/', (req, res) => res.send('Express Yo!'));
+app.get('/', (req, res) => res.send('Express DERPPP!'));
 
 app.listen(port, ()=> console.log(`Server is working and listening on port ${port}!`))
